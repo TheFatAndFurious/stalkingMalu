39e796ff6d2e6f32263f5fe48f2c5c9761073a23
mathieubaro <mrguerrilla@gmail.com>
2026-07-14 15:57:35 +0200

    device-contract

---
+++ fleet/packages/device-contract/src/lib/device-contract.spec.ts
@@ -0,0 +1,7 @@
+import { deviceContract } from './device-contract.js';
+
+describe('deviceContract', () => {
+  it('should work', () => {
+    expect(deviceContract()).toEqual('device-contract');
+  })
+})
---
+++ fleet/packages/device-contract/src/lib/device-contract.ts
@@ -0,0 +1,3 @@
+export function deviceContract(): string {
+  return 'device-contract';
+}
